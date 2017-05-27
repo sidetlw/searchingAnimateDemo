a60b0cc8a79a5a6b139ae032c45a11e712da2a64
sidetang <sidetang@tencent.com>
2017-05-27 14:59:55 +0800

    Initial Commit

---
+++ searchingAnimateDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  searchingAnimateDemo
+//
+//  Created by sidetang on 2017/5/27.
+//  Copyright © 2017年 sidetang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ searchingAnimateDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  searchingAnimateDemo
+//
+//  Created by sidetang on 2017/5/27.
+//  Copyright © 2017年 sidetang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
